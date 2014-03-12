81c42e8a93712757dc05ea0b9fb888f2d5f25c57
Marc Juchli <marc@mjnet.ch>
2014-03-11 19:02:43 -0700

    Display orders and it's details (after checkout)

--- app/controllers/OrdersController.scala
+++ app/controllers/OrdersController.scala
@@ -9,7 +9,16 @@ import play.api.data.Forms._
 import play.api.data.validation.Constraints._
 
 import models.Order
+import models.OrdersProduct
 
 object OrdersController extends Controller {
-  def newOrder = TODO
+
+  def list(customerId: Int) = Action {
+    Ok(views.html.orders.list(Order.getAllByCustomer(customerId)))
+  }
+
+  def detail(orderId: Int) = Action {
+    Ok(views.html.orders.detail(OrdersProduct.getAllByOrder(orderId)))
+  }
+
 }
